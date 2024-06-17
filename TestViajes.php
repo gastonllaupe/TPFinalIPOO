2067855cbac6e5b8a97b1480a0a93d1febf768c9
Facundo Ledesma <faculedesmabertalot@gmail.com>
2024-06-17 00:21:44 -0300

    Cambios dowhile test, agregado CRUD de ResponsableV

--- TestViajes.php
+++ TestViajes.php
@@ -7,30 +7,67 @@ include_once "Pasajero.php";
 include_once "ResponsableV.php";
 include_once "Viaje.php";
 
-// Menu
-function mostrarMenu() {
-    echo "\n--------- Menú ---------\n";
+// Menu Principal
+function menuPrincipal() {
+    echo "\n--------- Menú Principal ---------\n";
+    echo "1) Gestión de Empresa\n";
+    echo "2) Gestión de Viajes\n";
+    echo "3) Gestión de Pasajeros\n";
+    echo "4) Gestión de Responsable\n";
+    echo "5) Salir\n";
+    echo "------------------------\n";
+    echo "Seleccione una opción: ";
+}
+
+// Menú Empresa
+function menuEmpresa() {
+    echo "\n--------- Menú de Empresa ---------\n";
     echo "1) Crear Empresa\n";
-    echo "2) Crear Viaje\n";
-    echo "3) Ingresar Pasajero\n";
-    echo "4) Modificar Pasajero\n";
-    echo "5) Eliminar Pasajero\n";
-    echo "6) Ingresar Responsable\n";
-    echo "7) Modificar Responsable\n";
-    echo "8) Eliminar Responsable\n";
-    echo "9) Ver Viajes\n";
-    echo "10) Modificar Empresa\n";
-    echo "11) Modificar Viaje\n";
-    echo "12) Eliminar Empresa\n";
-    echo "13) Eliminar Viaje\n";
-    echo "14) Mostrar Detalles de un Viaje\n";
-    echo "15) Salir\n";
+    echo "2) Modificar Empresa\n";
+    echo "3) Eliminar Empresa\n";
+    echo "4) Volver al Menú Principal\n";
+    echo "------------------------\n";
+    echo "Seleccione una opción: ";
+}
+
+// Menú Viaje
+function menuViaje() {
+    echo "\n--------- Menú de Viaje ---------\n";
+    echo "1) Crear Viaje\n";
+    echo "2) Modificar Viaje\n";
+    echo "3) Eliminar Viaje\n";
+    echo "4) Listar Viaje\n";
+    echo "5) Ver Detalles de un Viaje\n";
+    echo "6) Volver al Menú Principal\n";
+    echo "------------------------\n";
+    echo "Seleccione una opción: ";
+}
+
+// Menú Pasajero
+function menuPasajero() {
+    echo "\n--------- Menú de Pasajero ---------\n";
+    echo "1) Ingresar Pasajero\n";
+    echo "2) Modificar Pasajero\n";
+    echo "3) Eliminar Pasajero\n";
+    echo "4) Listar Pasajeros\n";
+    echo "5) Volver al Menú Principal\n";
+    echo "------------------------\n";
+    echo "Seleccione una opción: ";
+}
+
+// Menú Responsable
+function menuResponsable() {
+    echo "\n--------- Menú de Responsable ---------\n";
+    echo "1) Ingresar Responsable\n";
+    echo "2) Modificar Responsable\n";
+    echo "3) Eliminar Responsable\n";
+    echo "5) Volver al Menú Principal\n";
     echo "------------------------\n";
     echo "Seleccione una opción: ";
 }
 
 
-// Funcion que crea la empresa
+// Funciones CRUD para la empresa
     function crearEmpresa() {
         $empresa = new Empresa();
         echo "Ingrese el nombre de la empresa: ";
@@ -123,12 +160,51 @@ function mostrarMenu() {
         } else {
             echo "Error al ingresar responsable: " . $responsable->getmensajeoperacion() . "\n";
         }
-    }    
+    }
+    
+    function modificarResponsable() {
+        echo "Ingrese el número de empleado del responsable a modificar: ";
+        $numEmpleado = trim(fgets(STDIN));
+        $responsable = new ResponsableV();
+
+        if ($responsable->buscar($numEmpleado)) {
+            echo "Ingrese el nuevo número de licencia: ";
+            $numLicencia = trim(fgets(STDIN));
+            echo "Ingrese el nuevo nombre: ";
+            $nombre = trim(fgets(STDIN));
+            echo "Ingrese el nuevo apellido: ";
+            $apellido = trim(fgets(STDIN));
+            $responsable->cargar($numEmpleado, $numLicencia, $nombre, $apellido);
+            
+            if ($responsable->modificar()) {
+                echo "Responsable modificado con éxito.\n";
+            } else {
+                echo "Error al modificar responsable: " . $responsable->getmensajeoperacion() . "\n";
+            }
+        } else {
+            echo "Responsable no encontrado.\n";
+        }
+    }
+
+    function eliminarResponsable() {
+        $responsable = new ResponsableV();
+        echo "Ingrese el número de documento del responsable a eliminar: ";
+        $nrodoc = trim(fgets(STDIN));
+        if ($responsable->buscar($nrodoc)) {
+            if ($responsable->eliminar()) {
+                echo "Viaje eliminado con éxito.";
+            } else {
+                echo "Error al eliminar viaje: " . $responsable->getmensajeoperacion();
+            }
+        } else {
+            echo "Viaje no encontrado.";
+        }
+    }
 
 
 
     // Funciones CRUD para Viaje
-    function crearViaje($destino, $cantMaxPasajeros, $idEmpresa, $numEmpleadoResponsable) {
+    function crearViaje() {
         $viaje = new Viaje();
         $viaje->cargar(null, $destino, $cantMaxPasajeros, $idEmpresa, $numEmpleadoResponsable);
         if ($viaje->insertar()) {
@@ -196,74 +272,174 @@ function mostrarMenu() {
     }
 
 
+// Funciones correspondientes a las opciones del Menú Principal
+
+function gestionEmpresas() {
 
     do {
-        mostrarMenu();
-        $opcion = trim(fgets(STDIN));
+        menuEmpresa();
+        $opcionEmpresa = trim(fgets(STDIN));
 
-        switch ($opcion) {
+        switch($opcionEmpresa) {
+
             case 1:
                 crearEmpresa();
                 break;
 
             case 2:
-                crearViaje();
-                break;
-
-            case 3:
-                ingresarPasajero();
-                break;
-            
-            case 4:
-                modificarPasajero();
-                break;
-
-            case 5:
-                eliminarPasajero();
-                break;
-
-            case 6:
-                ingresarResponsable();
-                break;
-
-            case 7:
-                modificarResponsable();
-                break;
-
-            case 8:
-                eliminarResponsable();
-                break;
-
-            case 9:
-                verViajes();
-                break;
-
-            case 10:
                 modificarEmpresa();
                 break;
 
-            case 11:
-                modificarViaje();
-                break;
-
-            case 12:
+            case 3:
                 eliminarEmpresa();
                 break;
-
-            case 13:
-                eliminarViaje();
-                break;
             
-            case 14:
-                mostrarDetallesViaje();
-                break;
-
-            case 15:
-                echo "Saliendo..\n";
+            case 4:
+                echo "Volviendo al Menú Principal\n";
                 break;
 
             default:
-                echo "Opción no válida. Por favor, intente de nuevo.\n";
+                echo "Opción inválida. Por favor, itnente de nuevo\n";
         }
+    } while ($opcionEmpresa != 4);
+} 
 
-    }   while ($opcion != 0);
+function gestionViajes() {
+    do {
+        menuViaje();
+        $opcionViaje = trim(fgets(STDIN));
+
+        switch($opcionViaje) {
+
+            case 1:
+                crearViaje();
+                break;
+
+            case 2:
+                modificarViaje();
+                break;
+
+            case 3:
+                eliminarViaje();
+                break;
+
+            case 4:
+                mostrarDetallesViaje();
+                break;
+
+            case 5:
+                echo "Volviendo al Menú Principal\n";
+                break;
+
+            default:
+            echo "Opción inválida. Por favor, itnente de nuevo\n";
+        }
+    } while ($opcionViaje != 5);
+}
+
+
+function gestionPasajeros() {
+    
+    do {
+        menuPasajero();
+        $opcionPasajero = trim(fgets(STDIN));
+
+        switch($opcionPasajero) {
+
+            case 1:
+                ingresarPasajero();
+                break;
+            
+            case 2:
+                modificarPasajero();
+                break;
+
+            case 3:
+                eliminarPasajero();
+                break;
+
+            case 4: 
+                listarPasajero();
+                break;
+
+            case 5:
+                echo "Volviendo al Menú Principal\n";
+                break;
+
+            default:
+            echo "Opción no válida. Por favor, intente de nuevo.\n";
+        }
+    } while ($opcionPasajero != 5);
+}
+
+
+function gestionResponsable() {
+
+    do {
+        menuResponsable();
+        $opcionResponsable = trim(fgets(STDIN));
+
+        switch ($opcionResponsable) {
+
+            case 1:
+                ingresarResponsable();
+                break;
+            
+            case 2:
+                modificarResponsable();
+                break;
+        
+            case 3:
+                eliminarResponsable();
+                break;
+
+            case 4:
+                echo "Volviendo al Menú Principal\n";
+                break;
+
+            default:
+            echo "Opción no válida. Por favor, intente de nuevo.\n"; 
+        }
+    } while ($opcionResponsable != 4);
+}
+
+
+
+// Función principal para mostrar y gestionar el menú principal
+
+function mostrarMenuPrincipal() {
+
+    do {
+        menuPrincipal();
+        $opcionPrincipal = trim(fgets(STDIN));
+
+        switch ($opcionPrincipal) {
+
+            case 1:
+                gestionEmpresas();
+                break;
+
+            case 2: 
+                gestionViajes();
+                break;
+
+            case 3: 
+                gestionPasajeros();
+                break;
+                
+            case 4:
+                gestionResponsable();
+                break;
+
+            case 5:
+                echo "Saliendo del Programa.\n";
+                break;
+
+            default:
+            echo "Opción no válida. Por favor, intente de nuevo.\n";
+        }
+    } while ($opcionPrincipal != 5);
+}
+
+// Ejecutar la función principal para iniciar el programa
+mostrarMenuPrincipal();
